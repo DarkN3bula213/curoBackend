14d32fbc46114052868dcca42a35c6d72f276094
madrigal1 <kurupgokul11@gmail.com>
2020-10-26 13:25:35 +0530

    feat:manager module

--- src/routes/v1/manager/fetchManagers.ts
+++ src/routes/v1/manager/fetchManagers.ts
@@ -1,8 +1,10 @@
 import asyncHandler from "../../../helpers/asyncHandler";
 import express from "express";
 import { ManagerRepo } from "../../../database/repository/ManagerRepo";
-import { InternalError } from "../../../core/ApiError";
+import { BadRequestError, InternalError } from "../../../core/ApiError";
 import { SuccessResponse } from "../../../core/ApiResponse";
+import validator from "../../../helpers/validator";
+import managerSchema from "./managerSchema";
 
 const router = express.Router();
 
@@ -14,6 +16,21 @@ router.get(
 
     new SuccessResponse("All Managers", {
       managers,
+    }).send(res);
+  })
+);
+
+router.get(
+  "/byMobileNumber",
+  validator(managerSchema.getInfo),
+  asyncHandler(async (req, res) => {
+    const { mobile_number } = req.body;
+
+    const Manager = await ManagerRepo.findByMobileNumber(mobile_number);
+    if (!Manager) throw new BadRequestError("Manager doesn't exist");
+
+    new SuccessResponse("Manager Info", {
+      Manager,
     }).send(res);
   })
 );
--- src/routes/v1/manager/index.ts
+++ src/routes/v1/manager/index.ts
@@ -1,13 +1,11 @@
 import express from "express";
 import newManger from "./newManager";
-import getInfo from "./getInfo";
 import toggleIsAllowed from "./toggleIsAllowed";
 import updateFcmToken from "./updateFcmToken";
 import fetchManagers from "./fetchManagers";
 const router = express.Router();
 
 router.use("/new", newManger);
-router.use("/getInfo", getInfo);
 router.use("/toggleIsAllowed", toggleIsAllowed);
 router.use("/updateFcmToken", updateFcmToken);
 router.use("/fetch", fetchManagers);
